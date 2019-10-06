cd43d608366b123d2c04884d63d95557d7258c80
unknown <nitishsahoo1994>
2019-10-06 12:00:19 +0530

    modify

--- restapp/views.py
+++ restapp/views.py
@@ -111,14 +111,14 @@ class EmployeeCRUDCBV(SerializeMixin,HttpResponseMixin,View):
         data=request.body
         valid_json=is_json(data)
         if not valid_json:
-            json_data=json.dumps({'msg','This is not a valid Json'})
+            json_data=json.dumps({'msg':'This is not a valid Json'})
             return self.render_to_http_response(json_data,status=404)
         pdata=json.loads(data)
         id=pdata.get('id',None)
         if id is not None:
             emp=self.get_object_by_id(id)
             if emp is None:
-                json_data = json.dumps({'msg', 'The request resource is not avialable with matched id:'})
+                json_data = json.dumps({'msg':'The request resource is not avialable with matched id:'})
                 return self.render_to_http_response(json_data, status=404)
             json_data=self.serialize([emp,])
             return self.render_to_http_response(json_data)
@@ -146,16 +146,16 @@ class EmployeeCRUDCBV(SerializeMixin,HttpResponseMixin,View):
         data=request.body
         valid_json=is_json(data)
         if not valid_json:
-            json_data = json.dumps({'msg', 'This is not a valid Json'})
+            json_data = json.dumps({'msg':'This is not a valid Json'})
             return self.render_to_http_response(json_data, status=404)
         pdata=json.loads(data)
         id=pdata.get('id',None)
         if id is None:
-            json_data = json.dumps({'msg', 'To perform updation id is mandatory,Please provide id'})
+            json_data = json.dumps({'msg':'To perform updation id is mandatory,Please provide id'})
             return self.render_to_http_response(json_data, status=404)
         emp=self.get_object_by_id(id)
         if emp is None:
-            json_data = json.dumps({'msg', 'The request source is not avialable with matched id:,Please provide valid id'})
+            json_data = json.dumps({'msg':'The request source is not avialable with matched id:,Please provide valid id'})
             return self.render_to_http_response(json_data, status=404)
         update_data=json.loads(data)
         origional_data={
@@ -168,7 +168,7 @@ class EmployeeCRUDCBV(SerializeMixin,HttpResponseMixin,View):
         form=EmployeeForm(origional_data,instance=emp)
         if form.is_valid():
             form.save(commit=True)
-            json_data=json.dumps({'msg','Updation is Success'})
+            json_data=json.dumps({'msg':'Updation is Success'})
             return self.render_to_http_response(json_data)
         if form.errors:
             json_data = json.dumps(form.errors)
